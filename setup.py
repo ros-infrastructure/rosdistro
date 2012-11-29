88ca6ef03d078e063cce4c257620ef80bb4d66ab
Wim Meeussen <wim@wim.gateway.2wire.net>
2012-11-29 13:03:30 -0800

    add command line tool

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='rosdistro',
-    version='0.0.11',
+    version='0.0.12',
     packages=['rosdistro'],
     package_dir = {'rosdistro':'src/rosdistro'},
-    scripts = ['scripts/rosdistro_generate_cache'],
+    scripts = ['scripts/rosdistro_generate_cache', 'scripts/rosdistro'],
     install_requires=['empy', 'PyYAML', 'argparse', 'rospkg', 'distribute'],
     package_data = {'rosdistro': ['resources/templates/*']},
     author='Wim Meeussen',
