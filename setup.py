1db3c6a4d951fdd12552fb29741be5e311b013bc
William Woodall <wjwwood@gmail.com>
2013-03-01 11:50:04 -0800

    0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rosdistro',
-    version='0.1.5',
+    version='0.1.6',
     packages=['rosdistro'],
     package_dir = {'rosdistro':'src/rosdistro'},
     scripts = ['scripts/rosdistro_generate_cache', 'scripts/rosdistro'],
