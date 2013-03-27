208b213876335b6be809d16922a5c57619dadd14
Dirk Thomas <dthomas@willowgarage.com>
2013-03-27 12:10:02 -0700

    0.1.13

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rosdistro',
-    version='0.1.12',
+    version='0.1.13',
     packages=['rosdistro'],
     package_dir = {'rosdistro':'src/rosdistro'},
     scripts = [
