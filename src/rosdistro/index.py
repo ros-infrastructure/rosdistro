a6a3eed24b87bac0c6887df5ca931903dfc5734e
Dirk Thomas <dthomas@willowgarage.com>
2013-04-19 17:48:22 -0700

    raise exception if distributions in index is not a list

--- src/rosdistro/index.py
+++ src/rosdistro/index.py
@@ -48,6 +48,10 @@ class Index(object):
 
         self.distributions = {}
         if 'distributions' in data and data['distributions']:
+            # if distributions is not a list raise an exception including the value
+            # this can be used to notify users (e.g. if an index.yaml file has been deleted / moved)
+            if not isinstance(data['distributions'], list):
+                raise RuntimeError('List of distributions is invalid: ' + data['distributions'])
             for distro_name in sorted(data['distributions']):
                 self.distributions[distro_name] = {}
                 distro_data = data['distributions'][distro_name]
