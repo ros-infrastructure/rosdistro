afe59f2bcbf315535b14d35e66a366146494be5d
Dirk Thomas <dthomas@willowgarage.com>
2013-04-22 14:17:57 -0700

    0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rosdistro',
-    version='0.1.18',
+    version='0.2.0',
     install_requires=['argparse', 'distribute', 'rospkg', 'PyYAML'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
