2b6abecd428201ba893f46b29b5e5859e4d756e6
Dirk Thomas <dthomas@willowgarage.com>
2013-04-24 18:17:57 -0700

    add missing dep on catkin_pkg

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='rosdistro',
     version='0.2.3',
-    install_requires=['argparse', 'distribute', 'rospkg', 'PyYAML'],
+    install_requires=['argparse', 'catkin_pkg', 'distribute', 'rospkg', 'PyYAML'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
     scripts=[
