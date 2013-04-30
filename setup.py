6e835511a8051c341397b6a1b5e4a4b2cc9eb0f6
Dirk Thomas <dthomas@willowgarage.com>
2013-04-29 23:30:31 -0700

    0.2.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rosdistro',
-    version='0.2.7',
+    version='0.2.8',
     install_requires=['argparse', 'catkin_pkg', 'distribute', 'rospkg', 'PyYAML'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
