00dde70d53f51c6f0363f2f1f2180ab75382d95f
Tom Moore <ayrton04@gmail.com>
2017-04-04 15:58:59 +0100

    Supporting GitHub auth tokens in manifest provider URLs

--- test/test_repository_specification.py
+++ test/test_repository_specification.py
@@ -19,3 +19,9 @@ def test_repository_specification():
 
     r.url = 'git@example.com:a/b/c/d/e.git'
     assert r.get_url_parts() == ('example.com', 'a/b/c/d/e')
+
+    r.url = 'https://1234567890abcdeABCDE1234567890abcdeABCDE@example.com:a/b/c/d/e.git'
+    parts = r.get_url_parts()
+    assert(len(parts) == 2)
+    assert(parts[0].endswith('example.com'))
+    assert(parts[1] == 'a/b/c/d/e')
