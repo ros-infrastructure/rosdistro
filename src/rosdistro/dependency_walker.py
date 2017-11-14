9ce5de716407d234a2a3b18aa090c22a706da789
Tracey Spicer <tspicer@clearpathrobotics.com>
2017-11-14 19:08:24 +0000

    Fix get_depends_on to work for Source Walker

--- src/rosdistro/dependency_walker.py
+++ src/rosdistro/dependency_walker.py
@@ -93,8 +93,7 @@ class DependencyWalker(object):
         for name in self._get_package_names():
             if name in ignore_pkgs:
                 continue
-            pkg = self._get_package(name)
-            repo = self._distribution_instance.repositories[pkg.repository_name].release_repository
+            repo = self._get_repo(name)
             if repo is None or repo.version is None:
                 continue
             deps = self._get_dependencies(name, depend_type)
@@ -126,6 +125,10 @@ class DependencyWalker(object):
         }
         return set([d.name for d in deps[dep_type]])
 
+    def _get_repo(self, name):
+        repo = self._distribution_instance.repositories[self._distribution_instance.release_packages[name].repository_name].release_repository
+        return repo
+
 
 class SourceDependencyWalker(DependencyWalker):
     def _get_package(self, pkg_name):
@@ -142,3 +145,7 @@ class SourceDependencyWalker(DependencyWalker):
 
     def _get_package_names(self):
         return self._distribution_instance.source_packages.keys()
+
+    def _get_repo(self, name):
+        repo = self._distribution_instance.repositories[self._distribution_instance.source_packages[name].repository_name].source_repository
+        return repo
