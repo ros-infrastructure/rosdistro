2565c2c5e4add200dc8828d738e7c3d2c1850485
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-04-23 10:46:04 -0700

    0.7.4

--- src/rosdistro/__init__.py
+++ src/rosdistro/__init__.py
@@ -61,7 +61,7 @@ from .manifest_provider.cache import CachedManifestProvider, CachedSourceManifes
 # same version as in:
 # - setup.py
 # - stdeb.cfg
-__version__ = '0.7.3'
+__version__ = '0.7.4'
 
 # index information
 
