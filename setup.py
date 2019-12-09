a7f1673f5391c86a2fddf123017e86a250752aab
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-12-09 11:42:07 -0800

    0.8.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ kwargs = {
     # same version as in:
     # - src/rosdistro/__init__.py
     # - stdeb.cfg
-    'version': '0.7.5',
+    'version': '0.8.0',
     'install_requires': ['PyYAML', 'setuptools'],
     'packages': find_packages('src'),
     'package_dir': {'': 'src'},
