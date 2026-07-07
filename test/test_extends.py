8ef74a4ecd9e0a08ae1c5a1087ff94d450953cb2
Federico Rossi <federico.rossi@creativa77.com.ar>
2026-07-07 11:26:26 -0300

    add test cases

---
+++ test/test_extends.py
@@ -0,0 +1,63 @@
+import os
+import pytest
+from rosdistro import get_distribution_file, get_index, CircularInheritanceError
+from . import path_to_url
+
+FILES_DIR = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'files'))
+
+
+def test_extends_parsing():
+    url = path_to_url(os.path.join(FILES_DIR, 'index_extends.yaml'))
+    index = get_index(url)
+
+    # Test base distro loading
+    base_dist = get_distribution_file(index, 'base')
+    assert base_dist.version == 3
+    assert 'bar_repo' in base_dist.repositories
+    assert base_dist.repositories['bar_repo'].origin_distro == 'base'
+
+    # Test derived distro loading and inheritance merging
+    derived_dist = get_distribution_file(index, 'derived')
+    assert derived_dist.version == 3
+
+    # Check extends attributes are parsed correctly
+    assert len(derived_dist.extends) == 1
+    assert derived_dist.extends[0]['distro_name'] == 'base'
+    assert derived_dist.extends[0]['extension_method'] == 'binary_import'
+
+    # Check dependencies are parsed correctly
+    assert len(derived_dist.dependencies) == 1
+    assert derived_dist.dependencies[0]['rosdep_sources_list_urls'] == [
+        'https://raw.githubusercontent.com/ros/rosdistro/master/rosdep/base.yaml'
+    ]
+    assert derived_dist.dependencies[0]['rosdep_minimum_target_platforms'] == [
+        'ubuntu:noble'
+    ]
+
+    # Check repositories merged
+    assert 'derived_repo' in derived_dist.repositories
+    assert 'bar_repo' in derived_dist.repositories
+
+    # Check metadata annotation
+    assert derived_dist.repositories['derived_repo'].origin_distro == 'derived'
+    assert derived_dist.repositories['derived_repo'].extension_method is None
+    assert derived_dist.repositories['bar_repo'].origin_distro == 'base'
+    assert derived_dist.repositories['bar_repo'].extension_method == 'binary_import'
+
+
+def test_circular_extends():
+    url = path_to_url(os.path.join(FILES_DIR, 'index_extends.yaml'))
+    index = get_index(url)
+
+    with pytest.raises(CircularInheritanceError) as excinfo:
+        get_distribution_file(index, 'cyclic_a')
+    assert "Circular inheritance detected" in str(excinfo.value)
+
+
+def test_platform_compatibility_warning(capsys):
+    url = path_to_url(os.path.join(FILES_DIR, 'index_extends.yaml'))
+    index = get_index(url)
+
+    get_distribution_file(index, 'derived_invalid_platform')
+    captured = capsys.readouterr()
+    assert "WARNING: Target platform 'ubuntu:quantal' specified in derived distribution is not supported by base distribution." in captured.out
